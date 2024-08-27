7f63ca21af40891fb6c19d83c66997c797f4c4f4
Facundo <facufazzi@gmail.com>
2024-08-27 19:40:52 -0300

    Globe Implementation

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/lib/utils";
 import { BackgroundGradientAnimation } from "./GradientBg";
+import { GridDemo } from "./GridGlobe";
 
 export const BentoGrid = ({
   className,
@@ -96,6 +97,8 @@ export const BentoGridItem = ({
             {title}
           </div>
         </div>
+
+        {id === 2 && <GridDemo/>}
       </div>
     </div>
   );
